9503828894e863a88e6175505249ade3ee021f03
cedric-andria <tantelycedric2608@gmail.com>
2024-01-26 17:25:03 +0300

    Update AdminNavbar.js

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -16,7 +16,6 @@
 
 */
 import { Link } from "react-router-dom";
-import popo from '../../assets/img/theme/team-4-800x800.jpg';
 // reactstrap components
 import {
   DropdownMenu,
@@ -65,7 +64,7 @@ const AdminNavbar = (props) => {
                   <span className="avatar avatar-sm rounded-circle">
                     <img
                       alt="..."
-                      src={popo.replace('admin/', '')}
+                      src={require("../../assets/img/theme/team-4-800x800.jpg".replace('admin', ''))}
                     />
                   </span>
                   <Media className="ml-2 d-none d-lg-block">
